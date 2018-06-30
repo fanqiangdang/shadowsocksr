f2e11ac9b1ff1790626a6f6b83512df08e688593
女生 <39326043+nvsheng@users.noreply.github.com>
2018-06-30 17:04:28 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ function index(page){
     window._G = window._G || {post: {}, postList: {}};
     $('title').html(_config['blog_name']);
     if(_G.postList[page] != undefined){
-      $('#container').html(_G.postList[page]);
+      $('container').html(_G.postList[page]);
       return;
     }
 
@@ -17,7 +17,7 @@ function index(page){
             per_page     : _config['per_page']
         },
         beforeSend:function(){
-          $('#container').html('<center><img src="loading.gif" class="loading"></center>');
+          $('container').html('<center><img src="loading.gif" class="loading"></center>');
         },
         success:function(data, textStatus, jqXHR){
             var link = jqXHR.getResponseHeader("Link") || "";
@@ -30,7 +30,7 @@ function index(page){
               prev = true;
             }
             var ractive = new Ractive({
-                template : '#listTpl',
+                template : 'listTpl',
                 data     : {
                     posts : data,
                     next  : next,
@@ -39,12 +39,12 @@ function index(page){
                 }
             });
             window._G.postList[page] = ractive.toHTML();
-            $('#container').html(window._G.postList[page]);
+            $('container').html(window._G.postList[page]);
 
             //将文章列表的信息存到全局变量中，避免重复请求
             for(i in data){
               var ractive = new Ractive({
-                  template : '#detailTpl',
+                  template : 'detailTpl',
                   data     : {post: data[i]}
               });
               window._G.post[data[i].number] = {};
@@ -80,9 +80,9 @@ function detail(id){
     }
     
     if(_G.post[id].body != undefined){
-      $('#container').html(_G.post[id].body);
+      $('container').html(_G.post[id].body);
       $('title').html(_G.post[id].title);
-      toggleDuoshuoComments('#container', id);
+      toggleDuoshuoComments('container', id);
       highlight();
       return;
     }
@@ -92,17 +92,17 @@ function detail(id){
             // access_token:_config['access_token']
         },
         beforeSend:function(){
-          $('#container').html('<center><img src="loading.gif" alt="loading" class="loading"></center>');
+          $('container').html('<center><img src="loading.gif" alt="loading" class="loading"></center>');
         },
         success:function(data){
             var ractive = new Ractive({
-                 el: "#container",
-                 template: '#detailTpl',
+                 el: "container",
+                 template: 'detailTpl',
                  data: {post: data}
             });
 
             $('title').html(data.title + " | " + _config['blog_name']);
-            toggleDuoshuoComments('#container', id);
+            toggleDuoshuoComments('container', id);
             highlight();
         }
     });  
